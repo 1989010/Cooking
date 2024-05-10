6030522687d26df2a326f56f91d6fdd0a8e4c85b
ckh <whrygns123@naver.com>
2024-05-10 14:01:56 +0900

    해당 카테고리의 리스트 클릭시 나오는 화면

---
+++ app/src/main/java/com/cookandroid/cooking/list_edit_salad.java
@@ -0,0 +1,14 @@
+package com.cookandroid.cooking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class list_edit_salad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_edit_salad);
+    }
+}
---
+++ app/src/main/java/com/cookandroid/cooking/list_edit_western.java
@@ -0,0 +1,14 @@
+package com.cookandroid.cooking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class list_edit_western extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_edit_western);
+    }
+}
