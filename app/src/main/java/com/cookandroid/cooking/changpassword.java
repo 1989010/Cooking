356beff89b3694dadd7c075f801955b8a0c668a9
ckh <whrygns123@naver.com>
2024-05-21 14:49:15 +0900

    프로필 화면 추가

---
+++ app/src/main/java/com/cookandroid/cooking/changpassword.java
@@ -0,0 +1,14 @@
+package com.cookandroid.cooking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class changpassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_changpassword);
+    }
+}
