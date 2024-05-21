a51213c95bfba21a85f9da43584eefbcc05c3604
ckh <whrygns123@naver.com>
2024-05-21 22:26:21 +0900

    게시글 화면 추가 및 수정

---
+++ app/src/main/java/com/cookandroid/cooking/add_mylist_list.java
@@ -0,0 +1,13 @@
+package com.cookandroid.cooking;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class add_mylist_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mylist_list);
+    }
+}
